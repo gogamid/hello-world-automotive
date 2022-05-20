e3741315b128d8de1b0aee57d559589922f2e9ca
Imron <imron.gamidli@lt.hs-fulda.de>
2022-05-20 13:51:24 +0200

    chore: add service class

---
+++ automotive/src/main/java/com/example/helloworldauto/HelloWorldService.kt
@@ -0,0 +1,15 @@
+package com.example.helloworldauto
+
+import androidx.car.app.CarAppService
+import androidx.car.app.Session
+import androidx.car.app.validation.HostValidator
+
+class HelloWorldService : CarAppService() {
+  override fun createHostValidator(): HostValidator {
+    TODO("Not yet implemented")
+  }
+
+  override fun onCreateSession(): Session {
+    TODO("Not yet implemented")
+  }
+}
