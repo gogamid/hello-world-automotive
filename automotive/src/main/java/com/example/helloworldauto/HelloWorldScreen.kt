616f06114ae5cc6f85f8a0b7aea92f4fd7aaffe7
Imron <imron.gamidli@lt.hs-fulda.de>
2022-05-20 14:08:20 +0200

    feat: implement screen class

--- automotive/src/main/java/com/example/helloworldauto/HelloWorldScreen.kt
+++ automotive/src/main/java/com/example/helloworldauto/HelloWorldScreen.kt
@@ -2,11 +2,14 @@ package com.example.helloworldauto
 
 import androidx.car.app.CarContext
 import androidx.car.app.Screen
-import androidx.car.app.model.Template
+import androidx.car.app.model.*
 
 class HelloWorldScreen(carContext: CarContext) : Screen(carContext) {
-  override fun onGetTemplate(): Template {
-    TODO("Not yet implemented")
-  }
 
+  override fun onGetTemplate(): Template {
+    val row = Row.Builder().setTitle("Hello World Automotive OS").build()
+    return PaneTemplate.Builder(Pane.Builder().addRow(row).build())
+      .setHeaderAction(Action.APP_ICON)
+      .build()
+  }
 }
