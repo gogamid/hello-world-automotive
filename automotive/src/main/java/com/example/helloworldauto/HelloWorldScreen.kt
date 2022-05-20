7c33969f13d2384c5e5e1f4f28ab73a540b2704b
Imron <imron.gamidli@lt.hs-fulda.de>
2022-05-20 14:04:52 +0200

    feat: implement service class and add screen class

---
+++ automotive/src/main/java/com/example/helloworldauto/HelloWorldScreen.kt
@@ -0,0 +1,12 @@
+package com.example.helloworldauto
+
+import androidx.car.app.CarContext
+import androidx.car.app.Screen
+import androidx.car.app.model.Template
+
+class HelloWorldScreen(carContext: CarContext) : Screen(carContext) {
+  override fun onGetTemplate(): Template {
+    TODO("Not yet implemented")
+  }
+
+}
--- automotive/src/main/java/com/example/helloworldauto/HelloWorldService.kt
+++ automotive/src/main/java/com/example/helloworldauto/HelloWorldService.kt
@@ -1,15 +1,27 @@
 package com.example.helloworldauto
 
+import android.content.Intent
+import android.content.pm.ApplicationInfo
 import androidx.car.app.CarAppService
+import androidx.car.app.R
+import androidx.car.app.Screen
 import androidx.car.app.Session
 import androidx.car.app.validation.HostValidator
 
 class HelloWorldService : CarAppService() {
   override fun createHostValidator(): HostValidator {
-    TODO("Not yet implemented")
+    return if (applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE != 0) {
+      HostValidator.ALLOW_ALL_HOSTS_VALIDATOR
+    } else HostValidator.Builder(applicationContext)
+      .addAllowedHosts(R.array.hosts_allowlist_sample)
+      .build()
   }
 
   override fun onCreateSession(): Session {
-    TODO("Not yet implemented")
+    return object : Session() {
+      override fun onCreateScreen(intent: Intent): Screen {
+        return HelloWorldScreen(carContext)
+      }
+    }
   }
 }
